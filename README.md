e0a2b1b343d726ff98b7cfea9856acfeae6346fc
Piotr Machowski <PiotrMachowski@users.noreply.github.com>
2020-06-12 01:22:29 +0200

    Added new services

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
+## Configuration
+
+In configuration.yaml:
+```yaml
+saver:
+```
+
+## Available services
+
 ### Save state
 ```yaml
 service: saver.save_state
@@ -14,5 +23,49 @@ data:
     - service: cover.set_cover_position
       data_template:
         entity_id: cover.living_room
-        position: "{{ attributes_current_position | int }}"
+        position: "{{ attr_current_position | int }}"
+    - service: input_text.set_value
+      data_template:
+        entity_id: input_text.cover_description
+        value: "Cover is now {{ state }}"
+```
+
+### Delete saved state
+```yaml
+service: saver.delete
+data:
+  entity_id: cover.living_room
+```
+
+### Set variable
+```yaml
+service: saver.set_variable
+data:
+  name: counter
+```
+
+### Delete variable
+```yaml
+service: saver.delete_variable
+data:
+  name: counter
+```
+
+### Execute script
+```yaml
+service: saver.execute
+data:
+  script:
+    - service: cover.set_cover_position
+      data_template:
+        entity_id: cover.living_room
+        position: "{{ cover_living_room_attr_current_position | int }}"
+    - service: input_text.set_value
+      data_template:
+        entity_id: input_text.cover_description
+        value: "Cover is now {{ cover_living_room_state }}"
+    - service: input_text.set_value
+      data_template:
+        entity_id: input_text.counter_description
+        value: "Counter has value {{ counter }}"
 ```
--- custom_components/saver/__init__.py
+++ custom_components/saver/__init__.py
@@ -6,84 +6,164 @@ from homeassistant.core import Context
 from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.script import Script
 from homeassistant.helpers.entity_component import EntityComponent
-from homeassistant.const import CONF_ENTITY_ID
+from homeassistant.const import CONF_ENTITY_ID, CONF_NAME
 
 import voluptuous as vol
 
 _LOGGER = logging.getLogger(__name__)
 
 DOMAIN = 'saver'
-CONF_RESTORE_SCRIPT = 'restore_script'
 
-SERVICE_SAVE_ATTRIBUTE = 'save_state'
-SERVICE_SAVE_ATTRIBUTE_SCHEMA = vol.Schema({
+CONF_DELETE_AFTER_RUN = 'delete_after_run'
+CONF_RESTORE_SCRIPT = 'restore_script'
+CONF_SCRIPT = 'script'
+CONF_VALUE = 'value'
+
+SERVICE_DELETE = 'delete'
+SERVICE_DELETE_SCHEMA = vol.Schema({
     vol.Required(CONF_ENTITY_ID): cv.entity_id
 })
 
-SERVICE_RESTORE_ATTRIBUTE = 'restore_state'
-SERVICE_RESTORE_ATTRIBUTE_SCHEMA = vol.Schema({
+SERVICE_DELETE_VARIABLE = 'delete_variable'
+SERVICE_DELETE_VARIABLE_SCHEMA = vol.Schema({
+    vol.Required(CONF_NAME): cv.string
+})
+
+SERVICE_EXECUTE = 'execute'
+SERVICE_EXECUTE_SCHEMA = vol.Schema({
+    vol.Required(CONF_SCRIPT): cv.SCRIPT_SCHEMA
+})
+
+SERVICE_RESTORE_STATE = 'restore_state'
+SERVICE_RESTORE_STATE_SCHEMA = vol.Schema({
     vol.Required(CONF_ENTITY_ID): cv.entity_id,
-    vol.Required(CONF_RESTORE_SCRIPT): cv.SCRIPT_SCHEMA
+    vol.Required(CONF_RESTORE_SCRIPT): cv.SCRIPT_SCHEMA,
+    vol.Optional(CONF_DELETE_AFTER_RUN, default=True): cv.boolean
+})
+
+SERVICE_SAVE_STATE = 'save_state'
+SERVICE_SAVE_STATE_SCHEMA = vol.Schema({
+    vol.Required(CONF_ENTITY_ID): cv.entity_id
+})
+
+SERVICE_SET_VARIABLE = 'set_variable'
+SERVICE_SET_VARIABLE_SCHEMA = vol.Schema({
+    vol.Required(CONF_NAME): cv.string,
+    vol.Required(CONF_VALUE): cv.string
 })
 
 
 def setup(hass, config):
     component = EntityComponent(_LOGGER, DOMAIN, hass)
-    saver_entity = SaverEntity(config[DOMAIN])
+    saver_entity = SaverEntity()
     component.add_entities([saver_entity])
+
+    def delete(call):
+        data = call.data
+        entity_id = data[CONF_ENTITY_ID]
+        saver_entity.delete(entity_id)
+
+    def delete_variable(call):
+        data = call.data
+        variable = data[CONF_NAME]
+        saver_entity.delete_variable(variable)
+
+    def execute(call):
+        data = call.data
+        script = data[CONF_SCRIPT]
+        saver_entity.execute(script)
+
+    def restore_state(call):
+        data = call.data
+        entity_id = data[CONF_ENTITY_ID]
+        restore_script = data[CONF_RESTORE_SCRIPT]
+        should_delete = data[CONF_DELETE_AFTER_RUN]
+        saver_entity.restore(entity_id, restore_script, should_delete)
 
     def save_state(call):
         data = call.data
         entity_id = data[CONF_ENTITY_ID]
         saver_entity.save(entity_id)
 
-    def restore_state(call):
+    def set_variable(call):
         data = call.data
-        entity_id = data[CONF_ENTITY_ID]
-        restore_script = data[CONF_RESTORE_SCRIPT]
-        saver_entity.restore(entity_id, restore_script)
+        name = data[CONF_NAME]
+        value = data[CONF_VALUE]
+        saver_entity.set_variable(name, value)
 
-    hass.services.register(DOMAIN, SERVICE_SAVE_ATTRIBUTE, save_state, SERVICE_SAVE_ATTRIBUTE_SCHEMA)
-    hass.services.register(DOMAIN, SERVICE_RESTORE_ATTRIBUTE, restore_state, SERVICE_RESTORE_ATTRIBUTE_SCHEMA)
+    hass.services.register(DOMAIN, SERVICE_DELETE, delete, SERVICE_DELETE_SCHEMA)
+    hass.services.register(DOMAIN, SERVICE_DELETE_VARIABLE, delete_variable, SERVICE_DELETE_VARIABLE_SCHEMA)
+    hass.services.register(DOMAIN, SERVICE_EXECUTE, execute, SERVICE_EXECUTE_SCHEMA)
+    hass.services.register(DOMAIN, SERVICE_RESTORE_STATE, restore_state, SERVICE_RESTORE_STATE_SCHEMA)
+    hass.services.register(DOMAIN, SERVICE_SAVE_STATE, save_state, SERVICE_SAVE_STATE_SCHEMA)
+    hass.services.register(DOMAIN, SERVICE_SET_VARIABLE, set_variable, SERVICE_SET_VARIABLE_SCHEMA)
 
     return True
 
 
 class SaverEntity(Entity):
 
-    def __init__(self, hass):
-        self._hass = hass
-        self._db = {}
+    def __init__(self):
+        self._entities_db = {}
+        self._variables_db = {}
 
     @property
     def name(self):
         return DOMAIN
 
-    def save(self, entity_id):
-        self._db[entity_id] = self.hass.states.get(entity_id)
+    def delete(self, entity_id):
+        self._entities_db.pop(entity_id)
         self.schedule_update_ha_state()
 
-    def restore(self, entity_id, restore_script):
-        if entity_id not in self._db:
+    def delete_variable(self, variable):
+        self._variables_db.pop(variable)
+        self.schedule_update_ha_state()
+
+    def execute(self, script):
+        script = Script(self.hass, script)
+        variables = {}
+        variables.update(self._variables_db)
+        for entity_id in self._entities_db:
+            variables.update(SaverEntity.convert_to_variables(self._entities_db[entity_id], entity_id))
+        script.run(variables=variables, context=Context())
+        self.schedule_update_ha_state()
+
+    def restore(self, entity_id, restore_script, delete):
+        if entity_id not in self._entities_db:
             return
-        old = self._db[entity_id]
+        old = self._entities_db[entity_id]
         variables = SaverEntity.convert_to_variables(old)
-        self._db.pop(entity_id)
+        if delete:
+            self._entities_db.pop(entity_id)
         script = Script(self.hass, restore_script)
         script.run(variables=variables, context=Context())
         self.schedule_update_ha_state()
 
+    def save(self, entity_id):
+        self._entities_db[entity_id] = self.hass.states.get(entity_id)
+        self.schedule_update_ha_state()
+
+    def set_variable(self, variable, value):
+        self._variables_db[variable] = value
+        self.schedule_update_ha_state()
+
     @property
     def state_attributes(self):
-        return {"entities": self._db}
+        return {
+            "entities": list(self._entities_db.keys()),
+            "variables": list(self._variables_db.keys())
+        }
 
     @property
     def state(self):
-        return len(self._db)
+        return len(self._entities_db) + len(self._variables_db)
 
     @staticmethod
-    def convert_to_variables(state):
-        variables = {"state": state.state}
+    def convert_to_variables(state, entity_id=None):
+        prefix = ""
+        if entity_id is not None:
+            prefix = f"{entity_id}_".replace(".", "_")
+        variables = {f"{prefix}state": state.state}
         for attr in state.attributes:
-            variables[f"attributes_{attr}"] = state.attributes[attr]
+            variables[f"{prefix}attr_{attr}"] = state.attributes[attr]
         return variables
