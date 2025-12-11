0f234631c774424a9d58f50d10455ecca89c03fb
Piotr Machowski <PiotrMachowski@users.noreply.github.com>
2025-12-11 05:28:15 +0100

    Add restoring entity states, remove deprecated services

--- custom_components/saver/const.py
+++ custom_components/saver/const.py
@@ -1,9 +1,11 @@
 import homeassistant.helpers.config_validation as cv
 from homeassistant.const import CONF_ENTITY_ID, CONF_NAME
+from homeassistant.components.light import ATTR_TRANSITION, VALID_TRANSITION
 
 import voluptuous as vol
 
 DOMAIN = 'saver'
+NAME = 'Saver'
 
 SAVER_SCHEMA = vol.Schema(
     {
@@ -16,6 +18,7 @@ CONF_DELETE_AFTER_RUN = 'delete_after_run'
 CONF_RESTORE_SCRIPT = 'restore_script'
 CONF_SCRIPT = 'script'
 CONF_VALUE = 'value'
+CONF_REGEX = 'regex'
 
 SERVICE_CLEAR = 'clear'
 SERVICE_CLEAR_SCHEMA = vol.Schema({
@@ -31,6 +34,11 @@ SERVICE_DELETE_VARIABLE_SCHEMA = vol.Schema({
     vol.Required(CONF_NAME): cv.string
 })
 
+SERVICE_DELETE_VARIABLE_REGEX = 'delete_variable_regex'
+SERVICE_DELETE_VARIABLE_REGEX_SCHEMA = vol.Schema({
+    vol.Required(CONF_REGEX): cv.string
+})
+
 SERVICE_EXECUTE = 'execute'
 SERVICE_EXECUTE_SCHEMA = vol.Schema({
     vol.Required(CONF_SCRIPT): cv.SCRIPT_SCHEMA
@@ -38,9 +46,9 @@ SERVICE_EXECUTE_SCHEMA = vol.Schema({
 
 SERVICE_RESTORE_STATE = 'restore_state'
 SERVICE_RESTORE_STATE_SCHEMA = vol.Schema({
-    vol.Required(CONF_ENTITY_ID): cv.entity_id,
-    vol.Required(CONF_RESTORE_SCRIPT): cv.SCRIPT_SCHEMA,
-    vol.Optional(CONF_DELETE_AFTER_RUN, default=True): cv.boolean
+    vol.Required(CONF_ENTITY_ID): cv.entity_ids,
+    vol.Optional(CONF_DELETE_AFTER_RUN, default=True): cv.boolean,
+    vol.Optional(ATTR_TRANSITION, default=None): vol.Any(VALID_TRANSITION, None),
 })
 
 SERVICE_SAVE_STATE = 'save_state'
